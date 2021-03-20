1f2fb268df67f574f396d3bcbd7a871e3ce21d53
jaflaten <ja.flaten91@gmail.com>
2021-03-20 14:11:30 +0100

    MOVE-2144 Added tests for GpgServiceImpl

--- src/main/java/no/difi/move/deploymanager/service/codesigner/GpgServiceImpl.java
+++ src/main/java/no/difi/move/deploymanager/service/codesigner/GpgServiceImpl.java
@@ -2,6 +2,7 @@ package no.difi.move.deploymanager.service.codesigner;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.difi.move.deploymanager.action.DeployActionException;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.operator.bc.BcPGPContentVerifierBuilderProvider;
 import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
@@ -11,6 +12,9 @@ import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 @Service
 @Slf4j
@@ -19,6 +23,9 @@ public class GpgServiceImpl implements GpgService {
 
     @Override
     public boolean verify(String signedDataFilePath, String downloadedSignature, String downloadedPublicKey) {
+        if(isNullOrEmpty(signedDataFilePath) || isNullOrEmpty(downloadedSignature) || isNullOrEmpty(downloadedPublicKey)) {
+            throw new IllegalArgumentException();
+        }
         try (InputStream signedData = new FileInputStream(signedDataFilePath);
              InputStream signature = new ByteArrayInputStream(downloadedSignature.getBytes());
              InputStream publicKey = new ByteArrayInputStream(downloadedPublicKey.getBytes())) {
@@ -27,7 +34,8 @@ public class GpgServiceImpl implements GpgService {
             PGPObjectFactory pgpFactory = new PGPObjectFactory(PGPUtil.getDecoderStream(signature), new JcaKeyFingerprintCalculator());
             PGPSignature sig = ((PGPSignatureList) pgpFactory.nextObject()).get(0);
             PGPPublicKeyRingCollection pgpPubKeyRingCollection = new PGPPublicKeyRingCollection(PGPUtil.getDecoderStream(publicKey), new JcaKeyFingerprintCalculator());
-            PGPPublicKey key = pgpPubKeyRingCollection.getPublicKey(sig.getKeyID());
+            PGPPublicKey key = Optional.ofNullable(pgpPubKeyRingCollection.getPublicKey(sig.getKeyID()))
+                    .orElseThrow(() -> new DeployActionException("Signer public key not found in keyring"));
 
             sig.init(new BcPGPContentVerifierBuilderProvider(), key);
             byte[] buffer = new byte[1024];
